4bc6b8d3543ece847919b20a99b642fc199a5da3
Tadeus Gary Wijono <e0656936@u.nus.edu>
2024-03-13 23:34:25 +0800

    Finished Lecture7, Added a lot of Comments

--- server/server.js
+++ server/server.js
@@ -1,9 +1,109 @@
+//require express package, import express package
+//express is like a framework, which you can install a lot of middleware
+//middleware for static display (html), middleware to run GraphQL, middleware for DB (MongoDB)
+//middleware run on top of Express, so that it can be understood by Node ecosystem
+//Node ecosystem is the one help so your server can run JavaScript (usually JavaScript could be run in browser)
 const express = require('express');
 
+const { MongoClient } = require('mongodb');
+
+//added below 2 lines for doing the GraphQl part
+//fs is file system packet, we need file-read operation
+const { ApolloServer } = require('apollo-server-express');
+const fs = require('fs');
+
+let db;
+async function connectToDb() {
+	//Marvel is the database name we created, case-sensitive
+	const url = 'mongodb://localhost/Marvel';
+	const client = new MongoClient(url, { useNewUrlParser: true });
+	//actually made the connection
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB at', url);
+        db = client.db();
+        console.log(await db.collection('characters').find({}).toArray());
+    } catch (error) {
+        console.error('Failed to connect to MongoDB', error);
+        process.exit(1); // Exit the process with an error code
+    }
+}
+
+//instantiate the express?
 const app = express();
 
+//static web server, display static (not changing) things
+//express going to run in public fodler
+//index.html and app.js is in public holder
 app.use(express.static('public'));
 
+/*************** GRAPHQL SETUP START ***************/
+
+//resolve the function when the query are called
+const resolvers = {
+	Query: {
+		getAllCharactersinfo,
+		getCharacterinfo,
+	},
+	Mutation: {
+		addCharacter
+	}
+};
+
+async function getAllCharactersinfo()
+{
+	  result = await db.collection('characters').find({}, {name:1, gender:1}).toArray();
+	  console.log(result)
+	  return result
+};
+
+//the first argument is to be revoked, hence we need to create a placeholder to inform the code its actually empty
+async function getCharacterinfo(_, args)
+{
+	  //cname is lcoal variable, get from the argument (from the user)
+	  cname = args.name
+	  console.log(cname)
+	  //dont actually need to convert to Array, but here Prof just copy from somewhere he said
+	  result = await db.collection('characters').find({name: cname}, {name:1, gender:1}).toArray();
+	  console.log(result)
+	  return result[0]
+};
+
+async function addCharacter(_, args)
+{
+	//to know and check in this terminal, is the args shown correctly
+	console.log(args)
+	const newcharacter = {'name': args.name, 'gender': args.gender}
+	//console.log(newcharacter)
+	result = await db.collection('characters').insertOne(newcharacter);
+	//can also print the result here, to make sure the insert is correct
+	return true
+};
+
+//Create GQL
+const server = new ApolloServer({
+	typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'),
+	resolvers,
+	//3 lines below is just for error handling
+	formatError: error => {
+	  console.log(error);
+	  return error;
+	},
+  });
+  //this will make your graphql run on localhost:3000/graphql
+  server.applyMiddleware({ app, path: '/graphql' });
+
+/*************** GRAPHQL SETUP ENDS ***************/
+
+
+//might not be okay to called in directly here as it is async function
+(async function () {
+await connectToDb();
+
+//where you actually start the server
+//below is anonymous function without name, but can actually called any function
 app.listen(3000, function () {
-	  console.log('App started on port 3000');
+	  console.log('App started on port 3000 successfully already');
 });
+
+})();
--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,77 @@
-class IssueFilter extends React.Component {
-	  render() {
-		      const systemnameinfilter = this.props.name;
-		      return (
-			            <div>This is a placeholder for the issue filter in {systemnameinfilter}.</div>
-			          );
-		    }
-}
+async function fetchGraphQLData(url, query, variables={}) {
+	const response = await fetch(url, {
+	  method: 'POST',
+	  headers: {
+		'Content-Type': 'application/json',
+		// Below here should also be used to write the API key, find out the header name, and the API key got it from vendor
+		// Add any additional headers here (e.g., Authorization for protected endpoints)
+	  },
+	  body: JSON.stringify({
+		query,
+		// variables: {}, // Uncomment and use if your query requires variables
+	  }),
+	});
+  
+	if (!response.ok) {
+	  throw new Error(`Network response was not ok: ${response.statusText}`);
+	}
+  
+	const resultjson = await response.json();
+	//console.log(resultjson)
+	return resultjson.data
+  }
 
-class IssueTable extends React.Component {
-	  render() {
-		      const systemnameintable = this.props.name;
-		      return (
-			            <div>This is a placeholder for a table of issues in {systemnameintable}.</div>
-			          );
-		    }
-}
+class GraphQL_API_Lecture7 extends React.Component {
 
-class IssueAdd extends React.Component {
-	  
-	  render() {
-		      const systemnameinadd = this.props.name;
-		      return (
-			            <div>This is a placeholder for a form to add an issue in {systemnameinadd}.</div>
-			          );
-		    }
-}
+	  constructor()
+	  {
+		super();
+		this.state = {anime: []}
+	  }
 
-class IssueList extends React.Component {
+	  componentDidMount()
+	  {
+		//typically we dont call the fetchGraphQLData inside here directly, here is more generic
+		this.loadData()
+	  }
+
+	  async loadData()
+	  {
+
+		const query = `{
+			Page {
+			  media {
+				siteUrl
+				title {
+				  english
+				  native
+				}
+				description
+			  }
+			}
+		  }`;
+
+		const data = await fetchGraphQLData ("https://graphql.anilist.co", query)
+		console.log(data.Page.media)
+		var listofanime = []
+		//data.Page.media.forEach ( item => {console.log(item.title.english)})
+		data.Page.media.forEach ( item => {listofanime.push(item.title.english)})
+		this.setState({anime: listofanime})
+	  }
+
 	  render() {
 		      const systemname = "IT5007 Bug Tracker";
 		      return (
-			            <React.Fragment>
-			              <h1>{systemname}</h1>
-			              <IssueFilter name={systemname}/>
-			              <hr />
-			              <IssueTable name={systemname}/>
-			              <hr />
-			              <IssueAdd name={systemname}/>
-			              <hr/>
-			            </React.Fragment>
+			            <div>
+							<h1>GraphQL API Test</h1>
+							{/*DOM in the React need unique key, otherwise some broswer will not render it, and throw error*/}
+							{/*if you write item without curly bracket, it will treated as string, and print item multiple times*/}
+							{this.state.anime.map( (item, pos) => <p key={pos}> {item} </p> )}
+						</div>
 			          );
 		    }
 }
 
-const element = <IssueList />;
+const element = <GraphQL_API_Lecture7 />;
 
 ReactDOM.render(element, document.getElementById('contents'));
